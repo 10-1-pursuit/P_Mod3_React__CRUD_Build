38c451cef1f93b787ed50fbc0698062508a0f396
SDBoahen <SDBoahen@gmail.com>
2023-08-11 12:27:51 -0400

    Added .env and one test variable

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import { BrowserRouter as Router } from "react-router-dom"
 import './App.css'
 
 
+console.log( "Some Default Environment Keys: " , import.meta.env )
+
+
+
+
 function App() {
+
+  const var10_1 = import.meta.env.VITE_10_1_VAR
+  // console.log("One of Our Environment Variables: ", import.meta.env.VITE_10_1_VAR)
+  console.log("One of Our Environment Variables: ", var10_1)
 
 
   return (
@@ -13,6 +22,8 @@ function App() {
       <Router>
 
         <h1>ScreenViews</h1>
+
+        <h2>-TEST-{ var10_1 }</h2>
 
       </Router>
     </>
